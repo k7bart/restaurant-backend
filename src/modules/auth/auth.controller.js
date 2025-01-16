b6065602bf3bdaeb223047aeed7afc830da11414
Kateryna Bartienieva <k7bart@gmail.com>
2025-01-17 00:12:55 +0200

    add login controller

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,5 +1,29 @@
 const authService = require("./auth.service");
 
+const COOKIE_OPTIONS = {
+  httpOnly: true,
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const { accessToken, refreshToken } = await authService.login(
+    email,
+    password
+  );
+
+  res.cookie("accessToken", accessToken, {
+    ...COOKIE_OPTIONS,
+    maxAge: 15 * 60 * 1000,
+  }); // 15 min
+  res.cookie("refreshToken", refreshToken, {
+    ...COOKIE_OPTIONS,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  }); // 7 days
+
+  return res.status(200).json({ message: "Login successful" });
+};
+
 const signup = async (req, res) => {
   const { firstName, lastName, phone, email, password } = req.body;
 
@@ -15,5 +39,6 @@ const signup = async (req, res) => {
 };
 
 module.exports = {
+  login,
   signup,
 };
